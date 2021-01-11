63282705fcf47abdc96373a219846327038f8c1f
github username <github email address>
2021-01-11 17:31:15 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Write your code here!
+
+document.querySelector("#main").remove();
+let newHeader = document.createElement('h1')
+newHeader.id = 'victory'
+newHeader.innerHTML = 'Avelon is the champion'
+
+// let main = document.querySelector('main#main');
+// main.remove();
+// let newHeader = document.createElement('h1');
+// newHeader.id = "victory";
+// newHeader.innerHTML = "is the champion"; 
+
+// document.getElementById('main').remove()
+// const h1 = document.createElement('h1')
+// document.body.appendChild(h1)
+// h1.id = 'victory'
+// const newHeader = document.querySelector('h1#victory')
+// // let name = "Avelon"
+// // newHeader.innerHTML = `${name} is the champion` 
+// newHeader.innerHTML = "Avelon is the champion"
